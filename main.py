407c0b46863a84349599d601017c31e80d3dbb8e
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-11-19 15:05:49 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# main.py
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+DOG_API_BASE_URL = "https://dog.ceo/api"
+
+@app.get("/api/v1/dogs")
+async def get_dogs():
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"{DOG_API_BASE_URL}/breeds/list/all")
+
+        # Check if the request was successful (status code 200)
+        response.raise_for_status()
+
+        data = response.json()
+
+        top_level_breeds = list(data["message"].keys())
+
+        return {"dogs": top_level_breeds}
+    except httpx.HTTPError as e:
+        raise HTTPException(status_code=e.response.status_code, detail="Error fetching data from Dog API")
